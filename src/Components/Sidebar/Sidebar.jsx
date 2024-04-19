4eaabfd51e0508c7c381680f1b540254cf452f2e
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-04-19 10:35:58 +0100

    added html structure and css of Sidebar components

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,12 +1,72 @@
-import React from 'react'
-import "./Sidebar.css"
+import React from "react";
+import "./Sidebar.css";
+// import Logo from '../../assets/Logo.svg'
+import logo from "../../assets/logo.png";
 
 const Sidebar = () => {
+  const date = new Date()
+  const year = date.getFullYear()
   return (
-    <div>
-        Sidebar
-    </div>
-  )
-}
+    <aside className="aside">
+      <a href="#" className="nav__logo">
+        <img src={logo} alt="" />
+      </a>
 
-export default Sidebar
+      <nav className="nav">
+        <div className="nav__menu">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="icon-home"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="icon-user-following"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="icon-briefcase"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#resume" className="nav__link">
+                <i className="icon-graduation"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="icon-layers"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#blog" className="nav__link">
+                <i className="icon-note"></i>
+              </a>
+            </li>
+             
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="icon-bubble"></i>
+              </a>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+
+      <div className="nav__footer">
+        <span className="copyright">&copy; {year}
+        </span>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
