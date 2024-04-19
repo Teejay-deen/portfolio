d087ab0a5fcb8c23c8b9287dc50a5c67f4e2a7c1
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-04-19 11:49:45 +0100

    html and css structure for home components

---
+++ src/Components/Home/HeaderSocial.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+const HeaderSocial = () => {
+  return (
+    <div className="home__socials">
+      <a
+        href="https://www.instagram.com/iam_confirm_mallam/"
+        className="home-socials-link"
+        target="_blank"
+      >
+        <i class="fa-brands fa-instagram"></i>
+      </a>
+
+      <a
+        href="https://www.twitter.com/m_modeller"
+        className="home-socials-link"
+        target="_blank"
+      >
+        <i class="fa-brands fa-twitter"></i>
+      </a>
+
+      <a
+        href="https://www.facebook.com/"
+        className="home-socials-link"
+        target="_blank"
+      >
+        <i class="fa-brands fa-facebook"></i>
+      </a>
+
+      <a
+        href="https://www.bahance.com"
+        className="home-socials-link"
+        target="_blank"
+      >
+        <i class="fa-brands fa-behance"></i>
+      </a>
+
+      <a
+        href="https://www.dribbble.com"
+        className="home-socials-link"
+        target="_blank"
+      >
+        <i class="fa-brands fa-dribbble"></i>
+      </a>
+
+      <a
+        href="https://www.pinterest.com"
+        className="home-socials-link"
+        target="_blank"
+      >
+        <i class="fa-brands fa-pinterest"></i>
+      </a>
+
+  
+    </div>
+  );
+};
+
+export default HeaderSocial;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import "./Home.css"
+import React from "react";
+import "./Home.css";
+import profile from "../../assets/Me.jpg";
+import HeaderSocial from "./HeaderSocial";
+import ScrollDown from "./ScrollDown";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={profile} alt="" className="home__img" />
+        <h1 className="home__name">Jimoh Tajudeen Olanrewaju</h1>
+        <span className="home__education">I am a Frontend Developer</span>
+        <HeaderSocial />
 
-export default Home
+        <a href="#contact" className="btn">
+          Hire Me
+        </a>
+        <ScrollDown />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
